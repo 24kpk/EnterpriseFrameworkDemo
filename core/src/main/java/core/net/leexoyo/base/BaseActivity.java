32c9151e1e1b1e4ba5669eccf2cfdd03ce184e72
冷寒 <24kpk@163.com>
2018-08-08 16:11:44 +0800

    修改基类

--- core/src/main/java/core/net/leexoyo/base/BaseActivity.java
+++ core/src/main/java/core/net/leexoyo/base/BaseActivity.java
@@ -22,6 +22,7 @@ public abstract class BaseActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(getContentViewId());
+
         ButterKnife.bind(this);
 
         mContext = this;
